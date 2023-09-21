ee6ff6b160c3080c604ef5e5f5d5e41312ff15d2
Greem <52639837+labradorescence@users.noreply.github.com>
2023-09-21 16:52:35 -0400

    Initialize 01-example-solution-intro-to-flask

---
+++ 01-intro-to-flask/server/debug.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from app import app
+from models import db, Production
+#This debug file can be used in place of flask shell
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace()
