218d875eb57dea982036c02c68006c136f118a1d
Greem <52639837+labradorescence@users.noreply.github.com>
2023-09-21 16:52:35 -0400

    Initialize 01-example-starter-intro-to-flask

---
+++ 01-intro-to-flask/server/seed.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# 📚 Review With Students:
+    # Seeding 
+# 5. ✅ Imports
+    # app from app
+    # db and Production from models
+
+# 6. ✅ Initialize the SQLAlchemy instance with `db.init_app(app)`
+
+
+# 7. ✅ Create application context `with app.app_context():`
+    # Info on application context: https://flask.palletsprojects.com/en/1.1.x/appcontext/
+
+# 8.✅ Create a query to delete all existing records from Production    
+   
+# 9.✅ Create some seeds for production and commit them to the database. 
+# 10.✅ Run in terminal:
+    # `python seed.py`
+# 11.✅ run `flask shell` in the terminal 
+    # from app import app
+    # from models import Production
+    # Check the seeds by querying Production
+# 12.✅ Navigate back to app.py  
+    
+    
