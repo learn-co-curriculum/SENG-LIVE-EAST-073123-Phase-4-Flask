dfd42a30be9ac5ba9001398035ab26e0e6b5c32b
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-10-04 14:13:42 -0400

    rest

--- 04-REST-APIs/server/app.py
+++ 04-REST-APIs/server/app.py
@@ -1,90 +1,83 @@
 from flask import Flask, jsonify, make_response, request 
 from flask_migrate import Migrate
-from models import db, Production
+from models import db, Production, Actor, Role
 
+
+from flask_restful import Api, Resource
 
 app = Flask(__name__) #Initialize the App
     
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'# Configure the database 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+#configures JSON responses to print on indented lines
 
 migrate = Migrate(app, db)  # Set the migrations  
 
 db.init_app(app) #initialize the application
 
-#Routes    
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
+api = Api(app) #initialize API
 
-@app.route('/lowest-budget-movies')
-def get_lowest_budget_movies():
-    movie = Production.query.order_by(Production.budget.desc()).first()
-    prod = {
-        "title": movie.title,
-        "genre": movie.genre,
-        "budget": movie.budget
-    }
-    return make_response(jsonify(prod), 200)
+class Productions(Resource): #this function name should be different from model class name that we are importing
+    #Create a route to /productions for GET requests
+    def get(self):
 
-#dynamic route
-@app.route('/productions/<string:title>')
+        #Create the query
+        quer = Production.query.all()
 
-def production(title):
+        #Loop through the query and convert each object into a dictionary 
+        prods = []
+        for each_p in quer:
+            prods.append({
+                "id": each_p.id,
+                "title": each_p.title,
+                "genre": each_p.genre,
+                "budget": each_p.budget
+            })
 
-    quer = Production.query.filter_by(title=title).first()
-    production={
-        "title": quer.title,
-        "genre": quer.genre,
-        "budget": quer.budget
-    }
-    return make_response(jsonify(production))
-    # `make_response` will allow us to make a response object with the response body and status code
-    # `jsonify` will convert our query into JSON
+        #Use make_response and jsonify to return a response
+        resp = make_response(jsonify(prods), 200)
+ 
+        return resp
+    
 
-    # `@app.route('/productions/<string:title>')
-    # def production(title):
-    #     production = Production.query.filter(Production.title == title).first()
-    #     production_response = {
-    #         "title":production.title,
-    #         "genre":production.genre,
-    #         "director": production.director
-    #         }
-    #     response = make_response(
-    #         jsonify(production_response),
-    #         200
-    #     )`    
+    def post(self):
+        #Get information from request.get_json()
+        request_json = request.get_json()
+        
+        #Create new object
+        new_production = Production(
+            title=request_json['title'],
+            genre=request_json['genre'],
+            budget=request_json['budget'],
+            image=request_json['image'],
+            director=request_json['director'],
+            description=request_json['description'],
+            ongoing=request_json['ongoing']
+        )
+        #Add and commit to db
+        db.session.add(new_production)
+        db.session.commit()
 
-# 16.✅ View the path and host with request context
+        #Convert to dictionary
+        prod_dict = {
+            "id": new_production.id,
+            "title": new_production.title,
+            "genre": new_production.genre,
+            "budget": new_production.budget}
 
-@app.route("/context")
-def context():
-    import ipdb;
-    # ipdb.set_trace()
+        #return as JSON
+        response = make_response(jsonify(prod_dict),
+            201
+        )
 
-    print("==========IN CONTEXT=======")
-    return f'''
-    <h1>
-        path   :   {request.path}
-    </h1>
-
-    <h1>
-        Host    :   {request.host}
-    </h1>
-    '''
-
-# 17.✅ Use the before_request request hook, what this hook does is up to you. You could hit a breakpoint, print something to server console or anything else you can think of.
-@app.before_request
-def runs_before():
-    cur_user = {"user_id": 3, "username": "spiderman"}
-    print("------ BEFORE REQUEST ------", cur_user)
-
-# Note: If you'd like to run the application as a script instead of using `flask run`, uncomment the line below 
-# and run `python app.py`
-
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
+        return response
+    
+api.add_resource(Productions, '/productions')
+#Test in Postman
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+#run `python app.py`
+#application as a script instead of using `flask run`
