4932edfad1dfcc7947f986a80a21953cdf0ddbaa
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-10-05 13:46:39 -0400

    rest2

---
+++ 05-REST-APIs2/README.md
@@ -0,0 +1,32 @@
+# Learning Goals
+- Build and Execute GET <:id> Requests
+- Discuss the Importance of Serializers
+- Demonstrate How to Properly Set Up Serializers
+- Demonstrate How to Use Serializers to Render Structured Data in API Responses
+- Continue to Reinforce RESTful Conventions When Naming Routes
+- Build and Execute PATCH / DELETE requests via Postman
+- Discuss Mass Assignment and How it Helps Improve Development Workflow
+- Discuss the Importance of Handling Exceptions in the Controller and Demonstrate How to Do So for at Least One Action
+
+# Lecture Topics
+- Get-one
+- SQLAlchemy-Serializer
+- Serializing Relationships
+- Serializing Associations
+
+
+## Why should we use serializer?
+
+### Current Problem:
+- We have access to the Project model, and we want to access associated models like Roles and Actors. While we could create another endpoint for the user to review data, using a serializer is a better approach.
+
+- Limitations of the `jsonify()` method include converting `SQLAlchemy objects` into `JSON objects` by obtaining a list of keys and values to pass to the client. This approach is not scalable, and jsonify can be finicky with Python objects.
+
+## The Solution:
+The solution lies in using `Flask-SQLAlchemy serializer`.
+
+## Why Should We Use a Serializer?
+ - Data Conversion: Serializers typically convert data into common formats like JSON (JavaScript Object Notation).
+ - Accessing Associated Data: Our current response body may not show the associated relationship, but by using a serializer, we can achieve this.
+
+ - SerializerMixin adds a `to_dict()` instance method, allowing us to utilize the `to_dict()` method in `app.py` and eliminating the need for repeatedly converting `SQLAlchemy objects` into `JSON objects`.
---
+++ 05-REST-APIs2/server/app.py
@@ -0,0 +1,151 @@
+from flask import Flask, jsonify, make_response, request 
+from flask_migrate import Migrate
+from models import db, Production, Actor, Role
+from flask_restful import Api, Resource
+#importing flask_restful 
+
+app = Flask(__name__) #Initialize the App
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'# Configure the database 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+#configures JSON responses to print on indented lines
+
+migrate = Migrate(app, db)  # Set the migrations  
+
+db.init_app(app) #initialize the application
+
+#initialize the API
+api = Api(app)
+
+#CRud
+class Productions(Resource): #should be different from the model 
+
+    #GET 
+    def get(self):
+        # #create a query
+        # quer = Production.query.all()
+
+        # # loop through the query and convert each obj into a dict
+        # prods = []
+
+        # for each_p in quer:
+        #     prods.append({
+        #         "id": each_p.id,
+        #         "title": each_p.title,
+        #         "genre": each_p.genre,
+        #         "budget": each_p.budget
+        #     })
+
+        prods = [ production.to_dict() for production in Production.query.all() ]
+
+        #return a response, add a status #
+        resp = make_response(jsonify(prods), 200)
+
+        return resp
+    
+    #post
+    def post(self):
+        request_json = request.get_json()
+
+        # create a new object 
+        new_production = Production(
+            title=request_json['title'],
+            genre = request_json['genre'],
+            budget=request_json['budget']    
+        )
+        # add / commit to db
+        db.session.add(new_production)
+        db.session.commit()
+
+        #convert to dictionary 
+        prod_dict = {
+            "id": new_production.id,
+            "title": new_production.title,
+            "genre": new_production.genre,
+            "budget": new_production.budget
+        }
+
+        #return as JSON
+        response = make_response(jsonify(prod_dict), 201)
+
+        return response
+
+api.add_resource(Productions, '/productions')
+
+#cRuD
+class ProductionsById(Resource):
+
+    def get(self, id):
+        #import ipdb; ipdb.set_trace()
+        id_prod = Production.query.filter(Production.id == id).one_or_none()
+
+        #handle message 
+        if id_prod is None: 
+            return make_response({"error": "Production Not Found"}, 404)
+        
+        id_prod_dict = {
+            "id": id_prod.id,
+            "title": id_prod.title,
+            "genre": id_prod.genre,
+            "budget": id_prod.budget
+        }
+
+        return make_response(id_prod_dict, 200)
+    
+    def delete(self, id):
+        #import ipdb; ipdb.set_trace()
+        id_prod = Production.query.filter_by(id=id).one_or_none()
+        #filtering elements from iterable objects, 
+        #filter_by is for ORM, SQLAlchemy. filters database based on attribute-value pairs 
+
+        if id_prod: 
+            db.session.delete(id_prod)
+            db.session.commit()
+
+            return make_response({"message": "deleted"}, 204)
+        
+        return make_response({"error": "Production Not Found"}, 404)
+
+api.add_resource(ProductionsById, "/productions/<int:id>")
+
+# @app.route('/productions/actors/<int:id>')
+# def production_actors_by_id(id):
+#     production = Production.query.filter(Production.id == id).first()
+#     prod_actors = []
+#     prod_actors.append(production.title)
+
+#     for role in production.roles:
+#         # actor = role.actor
+#         # actor_dict = actor.to_dict(rules=("-roles",))
+#         # prod_actors.append(actor_dict)
+#         prod_actors.append(role.actor.to_dict(rules=("-roles",)))
+
+#     return make_response(prod_actors, 200)
+
+# @app.route('/productions/actors/<int:id>')
+# def production_actors_by_id(id):
+#     production = Production.query.filter(Production.id == id).first()
+#     prod_actors = []
+#     prod_actors.append(production.title)
+
+#     actors = [ role.actor.to_dict(rules=("-roles",))
+#               for role in production.roles]
+
+#     prod_actors.append(actors)
+#     return make_response(prod_actors, 200)
+
+@app.route('/productions/actors/<int:id>')
+def production_actors_by_id(id):
+    production = Production.query.filter(Production.id == id).first()
+
+    actors = [ actor.to_dict(rules=("-roles",))
+              for actor in production.actors]
+
+    return make_response(actors, 200)
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+#run `python app.py`
+#application as a script instead of using `flask run`
